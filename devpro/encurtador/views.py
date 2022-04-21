af9cc1e1e6e6831476c49ef517c0733631eaaa88
Flavio <flavioantonio5@hotmail.com>
2022-04-20 21:08:31 -0300

    Css e Hmtl

--- devpro/encurtador/views.py
+++ devpro/encurtador/views.py
@@ -1,9 +1,19 @@
+from ast import Return
 from re import I, S
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
 from devpro.encurtador.models import UrlRedirect
+
+
+def relatorios(requisicao, slug):
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    url_reduzida = requisicao.build_absolute_uri(f'/{slug}')
+    contexto = {
+        'reduce': url_redirect, 
+        'url_reduzida': url_reduzida, }
+    return render(requisicao, 'encurtador/relatorio.html', contexto)
 
 
 def redirecionar(requisicao, slug):
